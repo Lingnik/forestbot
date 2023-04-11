af56688044d98566bd58dad9c439ac91f74c0746
Taylor Meek <Lingnik@users.noreply.github.com>
2023-04-10 18:21:22 -0700

    login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,10 +3,15 @@
 class SessionsController < ApplicationController
   def omniauth
     user = User.from_omniauth(request.env['omniauth.auth'])
+    puts user.inspect
+    puts user.valid?
+    puts user.errors.inspect
+    puts user.errors.full_messages.inspect
     if user.valid?
       session[:user_id] = user.id
       redirect_to root_path, notice: "Signed in successfully!"
     else
+      flash[:alert] = "You must log in with a valid cfs.eco email address"
       redirect_to '/login'
     end
   end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   get '/auth/:provider/callback', to: 'sessions#omniauth'
+  get '/login', to: 'home#login'
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
