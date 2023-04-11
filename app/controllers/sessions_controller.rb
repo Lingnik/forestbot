7d2a9ab508fd11f726578ac36144dea6201f2344
Taylor Meek <Lingnik@users.noreply.github.com>
2023-04-10 18:07:50 -0700

    omniauth attempt 3

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -10,11 +10,6 @@ class SessionsController < ApplicationController
       redirect_to '/login'
     end
   end
-  def create
-    @user = User.find_or_create_from_auth_hash(auth_hash)
-    session[:user_id] = @user.id
-    redirect_to root_path, notice: "Signed in successfully!"
-  end
 
   protected
 
--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,6 +1,10 @@
 # frozen_string_literal: true
 
+# config/initializers/omniauth.rb
+
 Rails.application.config.middleware.use OmniAuth::Builder do
-  #provider :developer if Rails.env.development?
-  provider :google_oauth2, ENV['GOOGLE_CLIENT_ID'], ENV['GOOGLE_CLIENT_SECRET']
+  provider :google_oauth2, ENV['GOOGLE_CLIENT_ID'], ENV['GOOGLE_CLIENT_SECRET'], {
+    scope: %w[email profile],
+    prompt: 'select_account'
+  }
 end
