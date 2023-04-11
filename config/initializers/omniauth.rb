ad72ee0f73fd9842a5e2653ac3e8154186d04fea
Taylor Meek <Lingnik@users.noreply.github.com>
2023-04-10 19:18:31 -0700

    moar omniauth

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -3,7 +3,7 @@
 # config/initializers/omniauth.rb
 
 Rails.application.config.middleware.use OmniAuth::Builder do
-  OmniAuth.config.allowed_request_methods = [:post]
+  OmniAuth.config.allowed_request_methods = [:post, :get]
 
   provider :google_oauth2, ENV['GOOGLE_CLIENT_ID'], ENV['GOOGLE_CLIENT_SECRET'], {
     scope: %w[email profile],
